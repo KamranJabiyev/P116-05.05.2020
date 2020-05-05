872fd4d24d1273a9ca0fca429eb6f92dd7976079
KamranJabiyev <kamran.jabiyev@code.edu.com>
2020-05-05 13:32:41 +0400

    operators

--- js/script.js
+++ js/script.js
@@ -1,1 +1,57 @@
-alert("P116");
+
+// var y=10;
+// x++;
+// x--;
+
+// x=30;
+
+// var x=20;
+
+// x+=100; //x=x+10
+
+// x-=5;
+
+// x*=2;
+
+// x/=5;
+
+// x%=7;
+
+// x**=2;
+
+// x%=7; //x=x%7
+
+// console.log(x);
+
+// var x=10;
+
+// var y=10;
+
+// console.log(x<=y);
+
+
+// var age=19;
+
+// var result=(age>18) ? "Xosh gelmisiniz":"Get boyuyende gelersen";
+
+// console.log(result);
+
+// if(age>18)
+// {
+//     if(age>30){
+//         console.log("Qocalmisan")
+//     }
+//     console.log("Xosh gelmisiniz");
+// }
+// else if(age==18)
+// {
+//     var name="Tural";
+
+//     console.log("Birazcada doz "+name);
+// }
+// else
+// {
+//     console.log("Get boyuyende gelersen");
+// }
+
+console.log( !( (15<14) || (20<21) && (13==12) ) );
